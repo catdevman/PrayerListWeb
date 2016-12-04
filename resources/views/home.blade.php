5298c2da71c2b5d7fa6dc5d2222ab06688cf1f78
Lucas Pearson <lpearson@five-startech.com>
2016-12-04 18:09:33 -0500

    Fixed up "add prayer" button on dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard <a href="{{ route('prayers.create')}}">Create Prayer</a></div>
+                <div class="panel-heading">Dashboard <a href="{{ route('prayers.create')}}"><span style="font-size: 2em;" class="glyphicon glyphicon-plus pull-right" title="Add Prayer" aria-hidden="true"></span></a></div>
                 @include('prayers.index')
             </div>
         </div>
